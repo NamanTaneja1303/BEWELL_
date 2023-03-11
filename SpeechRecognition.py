fd3baef4fb9c544737bdbc240cd4ec5e2fdc2fdd
NamanTaneja1303 <95750144+NamanTaneja1303@users.noreply.github.com>
2023-03-11 15:16:35 +0530

    SpeechRecognition

--- SpeechRecognition.py
+++ SpeechRecognition.py
@@ -13,3 +13,11 @@ while True:
 
         recognizer=speech_recognition.Recognizer()
         continue
+        //import speech_recognition as s
+#create a object of Recognizer
+sr=s.Recognizer()
+print("i am your script and listening you...........................")
+with s.Microphone() as m:
+    audio=sr.listen(m)
+    query=sr.recognize_google(audio,language='eng-in')
+    print(query)//
